7ba91e65605305215ff5a8b46443b0d14f9c36d7
ishtihoss <45158028+ishtihoss@users.noreply.github.com>
2019-09-11 00:44:21 +0800

    Delete Recommendation script.R
    deleting unnecessary file

--- Recommendation script.R
+++
@@ -1,9 +0,0 @@
-# Creating RMSE Function
-
-RMSE <- function(true_ratings, predicted_ratings){
-  sqrt(mean((true_ratings - predicted_ratings)^2))
-}
-
-# One-hot encoding
-
-cbind(small_edx, mtabulate(strsplit(small_edx$genres, ","))) %>% View()
